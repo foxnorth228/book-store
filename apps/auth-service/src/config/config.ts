046c57d791ddd97be249be5292c56dc7f5201c46
Alex Khitry <khitrii03@gmail.com>
2026-06-30 11:32:00 +0300

    fix: update auth-service architecture

--- apps/auth-service/src/config/config.ts
+++ apps/auth-service/src/config/config.ts
@@ -6,6 +6,11 @@ export const PATH_TO_PROJECT = "apps/auth-service";
 export const authServiceEnvSchema = envServerSchema.extend({
   GRPC_SERVER_HOST: z.coerce.string().default("0.0.0.0"),
   GRPC_SERVER_PORT: z.coerce.number().default(50051),
+
+  JWT_ACCESS_TOKEN_SECRET: z.string().min(32),
+  JWT_ACCESS_TOKEN_EXPIRES_IN: z.string(),
+  JWT_REFRESH_TOKEN_SECRET: z.string().min(32),
+  JWT_REFRESH_TOKEN_EXPIRES_IN: z.string(),
 });
 
 export type AuthServiceEnvSchema = typeof authServiceEnvSchema;
