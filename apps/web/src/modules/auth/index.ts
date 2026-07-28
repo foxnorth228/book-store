c1d50d84360e5e072c4af2265ed2a7ad950217aa
Alex Khitry <khitrii03@gmail.com>
2026-07-28 22:25:28 +0300

    feat: setup i18n for web

--- apps/web/src/modules/auth/index.ts
+++ apps/web/src/modules/auth/index.ts
@@ -1,3 +1,4 @@
+export { AuthTranslations } from "./locales";
 export { ForgotPasswordForm } from "./ui/AuthForm/ForgotPasswordForm";
 export { LoginForm } from "./ui/AuthForm/LoginForm";
 export { RegisterForm } from "./ui/AuthForm/RegisterForm";
---
+++ apps/web/src/modules/auth/locales/index.ts
@@ -0,0 +1,7 @@
+import AuthEnTranslations from "./en.json";
+import AuthRuTranslations from "./ru.json";
+
+export const AuthTranslations = {
+  en: AuthEnTranslations,
+  ru: AuthRuTranslations,
+};
