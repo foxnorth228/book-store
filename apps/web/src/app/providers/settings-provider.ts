25f55e353163fdfde0f7f9e4da81308871c4824b
Alex Khitry <khitrii03@gmail.com>
2026-08-14 16:36:08 +0300

    feat: update colors and add dark theme

--- apps/web/src/app/providers/settings-provider.ts
+++ apps/web/src/app/providers/settings-provider.ts
@@ -1,3 +1,4 @@
+import { Themes } from "@app/config/theme";
 import { useAppStore } from "@app/storage/use-app-store";
 import { FC, PropsWithChildren, useEffect } from "react";
 import { useTranslation } from "react-i18next";
@@ -5,12 +6,17 @@ import { useTranslation } from "react-i18next";
 export const SettingsProvider: FC<PropsWithChildren> = ({ children }) => {
   const { i18n } = useTranslation();
   const language = useAppStore((s) => s.language);
+  const theme = useAppStore((s) => s.theme);
 
   useEffect(() => {
     if (i18n.language !== language) {
       i18n.changeLanguage(language);
     }
   }, [i18n, language]);
+
+  useEffect(() => {
+    window.document.documentElement.classList.toggle("dark", theme === Themes.Dark);
+  }, [theme]);
 
   return children;
 };
